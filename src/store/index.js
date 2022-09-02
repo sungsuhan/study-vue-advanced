5f6a03e33c4118b15ce6dbe06537713a11fb86b0
sungsuhan <swhanssu@gmail.com>
2022-09-02 16:51:42 +0900

    store 모듈화

--- src/store/index.js
+++ src/store/index.js
@@ -1,58 +1,13 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import { fetchNewsList } from "@/api";
-import { fetchAskList } from "@/api";
-import { fetchJobsList } from "@/api";
+import mutations from "@/store/mutations";
+import actions from "@/store/actions";
+import state from "@/store/state";
 
 Vue.use(Vuex);
 
 export const store = new Vuex.Store({
-    state: {
-        news: [],
-        ask: [],
-        jobs: []
-    },
-    mutations: {
-        SET_NEWS(state, news) {
-            state.news = news;
-        },
-        SET_ASK(state, ask) {
-            state.ask = ask;
-        },
-        SET_JOBS(state, jobs) {
-            state.jobs = jobs;
-        }
-    },
-    actions: {
-        FETCH_NEWS(context) {
-            fetchNewsList()
-                .then(response => {
-                    console.log(response.data)
-                    context.commit('SET_NEWS', response.data)
-                })
-                .catch(error => {
-                    console.log(error)
-                })
-        },
-        FETCH_ASK(context) {
-            fetchAskList()
-                .then(response => {
-                    console.log(response.data);
-                    context.commit('SET_ASK', response.data);
-                })
-                .catch(error => {
-                    console.log(error)
-                })
-        },
-        FETCH_JOBS(context) {
-            fetchJobsList()
-                .then(response => {
-                    console.log(response.data);
-                    context.commit('SET_JOBS', response.data);
-                })
-                .catch(error => {
-                    console.log(error)
-                })
-        }
-    }
+    state,
+    mutations,
+    actions
 })
--- src/views/AskView.vue
+++ src/views/AskView.vue
@@ -1,11 +1,18 @@
 <template>
  <div>
-   <div v-for="ask in this.$store.state.ask" v-bind:key="ask.id">{{ ask.title }}</div>
+   <div v-for="ask in ask" v-bind:key="ask.id">{{ ask.title }}</div>
  </div>
 </template>
 
 <script>
+import { mapState } from 'vuex';
+
 export default {
+  computed: {
+    ...mapState({
+      ask: state => state.ask
+    })
+  },
   created() {
     this.$store.dispatch('FETCH_ASK');
   }
